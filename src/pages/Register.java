141a160bce8d2c34239d2a85c9d8b8463608ddce
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2022-05-19 11:11:39 +0200

    added the form

--- src/pages/Register.java
+++ src/pages/Register.java
@@ -5,18 +5,99 @@ import java.awt.*;
 import java.util.Objects;
 
 public class Register extends JFrame {
+    private final Color textColor = Color.decode("#283A6D");
+    private final Color bgColor = Color.decode("#F2F6FF");
+
     private JPanel panel;
 
-        public Register(){
-            super("Register");
-            createUIComponents();
-            setSize(1000, 600);
-            setLocationRelativeTo(null);
-            setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-            setVisible(true);
-            setContentPane(panel);
-            setIconImage(new ImageIcon(Objects.requireNonNull(getClass().getResource("/src/img/logo.png"))).getImage());
+    public Register() {
+        super("SignUp | Hiric");
+        createUIComponents();
+        setSize(1000, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+        initUI();
+    }
+
+    public void initUI() {
+        setIconImage(new ImageIcon(Objects.requireNonNull(getClass().getResource("/src/img/logo.png"))).getImage());
+
+        JPanel contentPanel = new JPanel(new BorderLayout());
+        JPanel headPanel = new JPanel(new BorderLayout());
+        JPanel bodyPanel = new JPanel(new GridBagLayout());
+
+        JButton registerButton = new JButton("Register");
+        JButton loginButton = new JButton("Login");
+
+        headPanel.setPreferredSize(new Dimension(0, 80));
+        headPanel.setBackground(this.bgColor);
+        bodyPanel.setBackground(this.bgColor);
+        JPanel headRightPanel = new JPanel(new GridLayout(1, 2, 15, 0));
+        JPanel headLeftPanel = new JPanel(new GridLayout(1, 1, 10, 3));
+        headRightPanel.setBackground(this.bgColor);
+        headLeftPanel.setBackground(this.bgColor);
+        headRightPanel.add(registerButton);
+        headRightPanel.add(loginButton);
+        headRightPanel.setBorder(BorderFactory.createEmptyBorder(20, 5, 20, 50));
+        headLeftPanel.setBorder(BorderFactory.createEmptyBorder(20, 50, 20, 5));
+
+        registerButton.setBackground(this.textColor);
+        registerButton.setForeground(Color.WHITE);
+        registerButton.setFocusPainted(false);
+        registerButton.setFont(new Font("nunito", Font.PLAIN, 15));
+
+        registerButton.setFocusPainted(false);
+        registerButton.setBorderPainted(false);
+        loginButton.setBackground(this.bgColor);
+        loginButton.setFont(new Font("nunito", 0, 17));
+        loginButton.setForeground(this.textColor);
+        loginButton.setBorder(null);
+        loginButton.setFocusPainted(false);
+
+        //add action on login and register buttons
+        loginButton.addActionListener(e -> {
+            Login login = new Login();
+            login.setVisible(true);
+            this.dispose();
+        });
+
+        registerButton.addActionListener(e -> {
+            Register register = new Register();
+            register.setVisible(true);
+            this.dispose();
+        });
+
+        JPanel bodyContent = new JPanel();
+        BoxLayout boxLayout = new BoxLayout(bodyContent, BoxLayout.Y_AXIS);
+
+        createUIComponents();
+        bodyContent.add(panel);
+
+        // end of login form
+
+        JLabel copyright = new JLabel("Copyright 2022 @husky | All Right Reserved.");
+        copyright.setFont(new Font("nunito", Font.ITALIC, 15));
+        copyright.setForeground(Color.GRAY);
+
+        JPanel footer = new JPanel(new GridBagLayout());
+        if (this.getSize().height == 600) {
+            footer.setBorder(BorderFactory.createEmptyBorder(235, 0, 0, 0));
         }
+        footer.add(copyright);
+        footer.setBackground(this.bgColor);
+        bodyContent.add(footer);
+
+        bodyContent.setLayout(boxLayout);
+        bodyContent.setBackground(this.bgColor);
+        bodyPanel.setBackground(this.bgColor);
+        bodyPanel.add(bodyContent);
+        headPanel.add(headLeftPanel, "West");
+        headPanel.add(headRightPanel, "East");
+        contentPanel.add(headPanel, "North");
+        contentPanel.add(bodyPanel, "Center");
+        this.add(contentPanel);
+    }
     private void createUIComponents() {
 
         JLabel accountTypeLabel = new JLabel("Account Type:");
