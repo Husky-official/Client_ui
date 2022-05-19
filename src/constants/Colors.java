29a7d264ae7c4d17fabc450953071cbc4bc66c8a
henriette-bizy <hopebiziyaremye@gmail.com>
2022-05-19 18:40:27 +0200

    feat: archives

--- src/constants/Colors.java
+++ src/constants/Colors.java
@@ -5,4 +5,7 @@ import java.awt.*;
 public class Colors {
     public Font SidebarFont = new Font("Nunito", Font.PLAIN,16);
     public Color onHover = new Color(63, 141, 251);
+    public Color ArchivesHeading = new Color(24, 28, 46);
+    public Color lightBlue = new Color(63, 141, 251);
+
 }
--- src/pages/Archives.java
+++ src/pages/Archives.java
@@ -1,9 +1,53 @@
 package src.pages;
 
+import src.constants.Colors;
+
 import javax.swing.*;
+import java.awt.*;
 
-public class Archives extends JLayeredPane {
+public class Archives extends JFrame {
 
-//jPanel heading = new JPanel()
+    Color titleColor = new Colors().ArchivesHeading;
+    Color lightblue = new Colors().lightBlue;
+    public Font setCustomFontSize(int size){
+        Font font =  new Colors().SidebarFont;
+        font = new Font("Nunito", Font.PLAIN,size);
+        return font;
+
+    }
+
+public Archives(){
+    this.setSize(1000, 600);
+    this.setLocationRelativeTo((Component)null);
+    this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    this.setLayout(null);
+    this.setVisible(true);
+
+
+    JPanel titlebar = new JPanel();
+    titlebar.setBounds(270,60,500,70);
+    JLabel heading = new JLabel("Husky's Archives",JLabel.CENTER);
+    heading.setForeground(titleColor);
+    heading.setFont(setCustomFontSize(19));
+    titlebar.add(heading);
+    this.add(titlebar);
+
+
+
+
+    JPanel tableTitle = new JPanel();
+    tableTitle.setBounds(250,120,950,50);
+    tableTitle.setBackground(lightblue);
+    this.add(tableTitle);
+
+
+
+}
+
+
+public static void main(String args[]){
+    new Archives();
+}
+
 
 }
