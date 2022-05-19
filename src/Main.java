53f86a6136aec5fb6385e65dd30855e12696a33d
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2022-05-19 19:17:25 +0200

    trying

--- src/Main.java
+++ src/Main.java
@@ -13,13 +13,14 @@ import java.util.Properties;
 
 public class Main extends JFrame {
 
-    FileReader fileReader = new FileReader("C:\\Users\\Nick\\Desktop\\Proj\\Client_ui\\resources\\application.properties");
-    Properties properties = new Properties();
+//    FileReader fileReader = new FileReader("C:\\Users\\Nick\\Desktop\\Proj\\Client_ui\\resources\\application.properties");
+//    Properties properties = new Properties();
 
     public Main() throws IOException {
 
-        properties.load(fileReader);
-        boolean logged_in = Boolean.parseBoolean(properties.getProperty("logged_in"));
+//        properties.load(fileReader);
+//        boolean logged_in = Boolean.parseBoolean(properties.getProperty("logged_in"));
+        boolean logged_in = true;
         System.out.println(logged_in);
 
         //change layout if user logged in
