a7b44dc8d37e9e59bccd8b8b43abf4cd1b6a6bd7
Gashugi Aderline <aderlinecarmella@gmail.com>
2022-05-19 14:37:31 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,9 @@ package src;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import src.client.ClientServerConnector;
 import src.models.RequestBody;
-import src.pages.NavBar;
-import src.pages.JobPostsView;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
 
 public class Main extends JFrame {
 
@@ -46,25 +41,6 @@ public class Main extends JFrame {
         String response = clientServerConnector.connect(request);
 
         label.setText(response);
-
-
-        NavBar navBar = new NavBar();
-//        BufferedImage imageIcon = ImageIO.read(new File("A:\\Projects\\Client_ui\\src\\img\\search.svg"));
-//        JPanel icon = new JPanel(){
-//            @Override
-//            protected void paintComponent(Graphics g){
-//                super.paintComponent(g);
-//                g.fillRoundRect(10, 10, 29 , 20, 2, 2);
-//            }
-//        };
-//        icon.setOpaque(true);
-//        icon.setBackground(Color.BLACK);
-//        icon.setBounds(100, 200, 2000, 500);
-//        this.add(icon);
-        this.add(navBar);
-
-        JobPostsView jobPostsView = new JobPostsView();
-        this.add(jobPostsView);
     }
 
     public static void main(String[] args) throws Exception {
