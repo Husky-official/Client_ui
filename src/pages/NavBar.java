67e8efda5f83c916b24e47326e5edf8e9ea3246c
Gashugi Aderline <aderlinecarmella@gmail.com>
2022-05-19 14:36:51 +0200

    Update NavBar.java

--- src/pages/NavBar.java
+++ src/pages/NavBar.java
@@ -1,49 +1,82 @@
 package src.pages;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.imageio.ImageIO;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.util.Objects;
 import javax.swing.*;
-import java.awt.image.*;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import javax.swing.border.Border;
 
-public class NavBar extends JLayeredPane {
+public class NavBar extends JFrame{
+    private final Color textColor = Color.decode("#283A6D");
+    private final Color bgColor = Color.decode("#F2F6FF");
+    private final Color background = Color.decode("#E3E3E3");
+    private JLabel notifications;
+    private JLabel settings;
+    private JLabel logout;
+    private JLabel recent;
+    private JLabel avatar;
+    private JTextField searchInput;
 
-    public NavBar() throws IOException {
-        this.setOpaque(true);
-        this.setBounds(100, 0, 900, 50);
-        this.setBackground(new Color(247, 247, 254));
-        Font textFont = new Font(Font.SANS_SERIF, Font.PLAIN, 17);
-        this.setLayout(null);
-
-        JPanel searchPane = new JPanel();
-        searchPane.isCursorSet();
-        searchPane.setBounds(130, 10, 200, 30);
-        searchPane.setBackground(new Color(237,237,247));
-
-
-        ImageIcon imageIcon = new ImageIcon("src/img/search.svg");
-        JLabel searchIcon = new JLabel(imageIcon);
-        searchIcon.setBounds(5,10,10,10);
-        searchIcon.setOpaque(true);
-
-        JLabel search = new JLabel("Search...");
-        search.setFont(textFont);
-        search.setForeground(Color.gray);
-        search.setBounds(50, 10, 100, 15);
-        
-        searchPane.add(search);
-        searchPane.add(searchIcon);
-        this.add(searchPane);
-//        this.add(searchIcon);
+    public NavBar() {
+        this.setTitle("Hiric");
+        this.setSize(1000, 600);
+        this.initUI();
+        this.setLocationRelativeTo((Component)null);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setVisible(true);
     }
-    public static void main(String[] args) throws IOException {
+
+    public void initUI() {
+        JPanel contentPanel = new JPanel(new BorderLayout());
+        JPanel headPanel = new JPanel(new BorderLayout());
+        JPanel bodyPanel = new JPanel(new GridBagLayout());
+        ImageIcon recentIcon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/src/img/recent.png")));
+        this.recent = new JLabel(recentIcon);
+        this.recent.setToolTipText("Recent Activities");
+        ImageIcon notificationsIcon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/src/img/notifications.png")));
+        this.notifications = new JLabel(notificationsIcon);
+        this.notifications.setToolTipText("Notifications");
+        ImageIcon settingsIcon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/src/img/settings.png")));
+        this.settings = new JLabel(settingsIcon);
+        this.settings.setToolTipText("Settings");
+        ImageIcon logoutIcon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/src/img/logout.png")));
+        this.logout = new JLabel(logoutIcon);
+        this.logout.setToolTipText("Logout");
+        ImageIcon avatarIcon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/src/img/avatar.png")));
+        this.avatar = new JLabel(avatarIcon);
+        this.avatar.setToolTipText("Profile");
+        this.searchInput = new JTextField("Search...");
+        this.searchInput.setBackground(this.bgColor);
+        this.searchInput.setPreferredSize(new Dimension(250, 30));
+        this.searchInput.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 5));
+        headPanel.setPreferredSize(new Dimension(0, 50));
+        headPanel.setBackground(this.background);
+        bodyPanel.setBackground(this.bgColor);
+        JPanel headRightPanel = new JPanel(new GridLayout(1, 4, 30, 0));
+        JPanel headLeftPanel = new JPanel();
+        headLeftPanel.setPreferredSize(new Dimension(500, 50));
+        headRightPanel.setBackground(this.background);
+        headLeftPanel.setBackground(this.background);
+        headRightPanel.add(this.notifications);
+        headRightPanel.add(this.settings);
+        headRightPanel.add(this.avatar);
+        headRightPanel.add(this.logout);
+        headLeftPanel.add(this.recent);
+        headLeftPanel.add(searchInput);
+        headRightPanel.setBorder(BorderFactory.createEmptyBorder(7, 5, 10, 20));
+        headLeftPanel.setBorder(BorderFactory.createEmptyBorder(7, 0, 10, 5));
+        headPanel.add(headLeftPanel, "West");
+        headPanel.add(headRightPanel, "East");
+        contentPanel.add(headPanel, "North");
+        this.add(contentPanel);
+    }
+
+    public static void main(String[] args) {
         new NavBar();
     }
-
-
-
 }
