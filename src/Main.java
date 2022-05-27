f3aec353cdecc0c62aab077e50a34a20752c981f
BillBenon <61540696+BillBenon@users.noreply.github.com>
2022-05-27 10:00:00 +0500

    merge with branch Chance

--- src/Main.java
+++ src/Main.java
@@ -22,11 +22,11 @@ public class Main extends JFrame {
 
         boolean logged_in = true;
         //change layout if user logged in
-//        if (logged_in){
+       if (logged_in){
             this.add(new DashboardPage());
-//        }else {
-//            this.add(new Home());
-//        }
+       }else {
+           this.add(new Home());
+       }
     }
 
     public static void main(String[] args) throws Exception {
